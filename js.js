9f1f7518fa2ee41629166705bc949af72ead7100
okellotechconsults <39102006+okellotechconsults@users.noreply.github.com>
2019-04-05 12:28:40 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,10 @@
 console.log("js.js is working");
+var config = {
+    apiKey: "AIzaSyAvXeHz88xqwaFA8kB9PCK_A6aUDE14kOo",
+    authDomain: "project-f7a5d.firebaseapp.com",
+    databaseURL: "https://project-f7a5d.firebaseio.com",
+    projectId: "project-f7a5d",
+    storageBucket: "project-f7a5d.appspot.com",
+    messagingSenderId: "79860480110"
+  };
+  firebase.initializeApp(config);
